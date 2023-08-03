72c18242d2e2beace47e787cc3ee1780cf32c1bc
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-08-03 00:50:34 -0700

    Topics learned on prototyping

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("Bacon");
+function User() {
+    this.name = "";
+    this.life = 100;
+    this.giveLife = function giveLife(targetPlayer) {
+        targetPlayer.life += 1;
+        console.log(this.name + " gave 1 life to target " + targetPlayer.name);
+    }
+}
+
+var Bucky = new User();
+var Wendy = new User();
+Bucky.name = "Bucky";
+Wendy.name = "Wendy";
+
+Bucky.giveLife(Wendy);
+console.log("Bucky: " + Bucky.life);
+console.log("Wendy: " + Wendy.life);
+
+// You can add functions to all objects
+User.prototype.uppercut = function uppercut(targetPlayer) {
+    targetPlayer.life -= 1;
+    console.log(this.name + " uppercutted " + targetPlayer.name);
+}
+
+Wendy.uppercut(Bucky);
+console.log("Bucky: " + Bucky.life);
+console.log("Wendy: " + Wendy.life);
+
+// You can add properties to all objects
+User.prototype.magic = 60;
+console.log(Bucky.magic);
+console.log(Wendy.magic);
